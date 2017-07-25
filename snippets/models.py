1a75f9420b8facbb3043b62dede80f4b67b0800a
sanghkaang <sanghkaang@gmail.com>
2017-07-25 10:58:59 +0900

    Fix type

--- snippets/models.py
+++ snippets/models.py
@@ -13,7 +13,7 @@ class Snippet(models.Model):
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    sytle = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
     class Meta:
         ordering = ('created',)
